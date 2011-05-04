823979817332b6c6e046954404ba100fe3aa5cee
Marcus Deglos <marcus@deglos.com>
2011-05-04 05:23:01 +0200

    Global change of every instance of 'linkback' to 'vinculum'.

--- vinculum_content_types.js
+++ vinculum_content_types.js
@@ -3,14 +3,14 @@
 Drupal.behaviors.contentTypes = {
   attach: function (context) {
     // Provide the vertical tab summaries.
-    $('fieldset#edit-linkback', context).drupalSetSummary(function(context) {
+    $('fieldset#edit-vinculum', context).drupalSetSummary(function(context) {
       // The summary is one of:
       // - Send and receive
       // - Send only
       // - Receive only
       // - Disabled
-      send = $("input[name='linkback_send']", context).is(':checked');
-      receive = $("input[name='linkback_receive']", context).is(':checked');
+      send = $("input[name='vinculum_send']", context).is(':checked');
+      receive = $("input[name='vinculum_receive']", context).is(':checked');
 
       if (send && receive) {
         summary = Drupal.t('Send and receive');
