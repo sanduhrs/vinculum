3f051b7d5f81f2b4ce57aa4e82c140406e6a44a5
Marcus Deglos <marcus@deglos.com>
2012-03-30 01:49:58 +0100

    Initial commit of 7.x-2.x

---
+++ README.txt
@@ -0,0 +1,8 @@
+
+TECHNICAL REFERENCES
+
+Pingback specification
+- http://www.hixie.ch/specs/pingback/pingback
+
+Trackback specification
+- http://www.sixapart.com/pronet/docs/trackback_spec
---
+++ vinculum.populate_test_data.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Root directory of Drupal installation.
+ */
+define('DRUPAL_ROOT', '/var/www/vhosts/dev');
+
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
+
+$examples = array();
+$examples[] = array(
+  'nid'       => 26,
+  'url'       => 'http://example.com/phpnw_commentary',
+  'title'     => 'Random musings on PHP NW',
+  'excerpt'   => NULL,
+  'handler'   => 'pingback',
+  'origin_ip' => '192.168.10.1',
+  'timestamp' => time() - rand(0, 86400),
+);
+$examples[] = array(
+  'nid'       => 26,
+  'url'       => 'http://bar.example.com/php_nw_was_good',
+  'title'     => 'Another great PHP NW',
+  'excerpt'   => NULL,
+  'handler'   => 'pingback',
+  'origin_ip' => '192.168.10.2',
+  'timestamp' => time() - rand(0, 86400),
+);
+$examples[] = array(
+  'nid'       => 21,
+  'url'       => 'http://zeus.example.com/zeus_all_bad',
+  'title'     => 'Is Zeus all bad?',
+  'excerpt'   => NULL,
+  'handler'   => 'pingback',
+  'origin_ip' => '192.168.10.3',
+  'timestamp' => time() - rand(0, 86400),
+);
+$examples[] = array(
+  'nid'       => 16,
+  'url'       => 'http://clamav.example.com/clamming_it_up',
+  'title'     => 'ClamAV for drupal',
+  'excerpt'   => NULL,
+  'handler'   => 'pingback',
+  'origin_ip' => '192.168.10.4',
+  'timestamp' => time() - rand(0, 86400),
+);
+
+
+foreach ($examples as $example) {
+  db_merge('node_vinculum_received')
+    ->key(array(
+      'nid' => $example['nid'],
+      'url' => $example['url'],
+      ))
+    ->fields(array(
+      'title'     => $example['title'],
+      'excerpt'   => $example['excerpt'],
+      'handler'   => $example['handler'],
+      'origin_ip' => $example['origin_ip'],
+      'timestamp' => $example['timestamp'],
+    ))
+    ->execute();
+}
